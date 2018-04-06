b42aeec871513061dae76b528bb3b34367f6630f
Mikey Mo <fadeout7@gmail.com>
2018-04-06 11:54:30 +0100

    Added more constants

--- vpaad/constants.py
+++ vpaad/constants.py
@@ -15,12 +15,19 @@ CANDLE_RES_TO_HISTORICAL_RES = {
     "30MINUTE": "30Min",
     "HOUR": "1H",
 }
+HISTORICAL_RES_TO_CANDLE_RES = {
+    value: key for key, value in CANDLE_RES_TO_HISTORICAL_RES.items()
+}
 CANDLE_RES_TO_TIMEDELTA = {
     "5MINUTE": datetime.timedelta(minutes=5),
     "1MINUTE": datetime.timedelta(minutes=1),
     "15MINUTE": datetime.timedelta(minutes=15),
     "30MINUTE": datetime.timedelta(minutes=30),
     "HOUR": datetime.timedelta(hours=1),
+}
+HISTORICAL_RES_TO_TIMEDELTA = {
+    key: CANDLE_RES_TO_TIMEDELTA[value]
+    for key, value in HISTORICAL_RES_TO_CANDLE_RES.items()
 }
 DATETIME_STR_FORMAT = "%Y-%m-%d %H:%M:%S"
 DF_DATETIME_FORMAT = "%Y:%m:%d-%H:%M:%S"
