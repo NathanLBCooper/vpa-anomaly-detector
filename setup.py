3887053b2eb7f7c23f75e72b19e83b681820a763
Mikey Mo <fadeout7@gmail.com>
2018-04-06 09:39:26 +0100

    Beginning to set up python package for vpaad

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Setup vpa_anomaly_detector
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='vpa_anomaly_detector',
+    author='Mikey Mo',
+    author_email='fadeout7@gmail.com',
+    url='https://github.com/mikeymo/vpa_anomaly_detector',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=('trading_id', 'pandas', 'numpy'),
+    namespace_packages=['vpaad'],
+    description=(
+        'Experimental tool for detect anomalies in markets using the '
+        'ig.com API and volume price analysis'
+    )
+)
